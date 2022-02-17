4555f89c2d66a6f8bc6d6ef52d2561ed966d047c
Quentin Neves <neves.quentin@gmail.com>
2022-02-17 15:58:45 +0100

    Remove inheritance between ProdDBContext and DevDbContext

--- Areas/Identity/Data/DevDbContext.cs
+++ Areas/Identity/Data/DevDbContext.cs
@@ -4,9 +4,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Mordle.Data;
 
-public class DevDbContext : ProdDbContext
+public class DevDbContext : IdentityDbContext<IdentityUser>
 {
-    public DevDbContext(DbContextOptions<DbContext> options, IConfiguration configuration) : base(options, configuration) { }
+    protected readonly IConfiguration _configuration;
+    public DevDbContext(IConfiguration configuration)
+    : base()
+    {
+        _configuration = configuration;
+    }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -14,11 +19,5 @@ public class DevDbContext : ProdDbContext
         // Customize the ASP.NET Identity model and override the defaults if needed.
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
-    }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-    {
-        // connect to sql server database
-        options.UseSqlite(_configuration.GetConnectionString("WebApiDatabase"));
     }
 }
--- Areas/Identity/Data/ProdDbContext.cs
+++ Areas/Identity/Data/ProdDbContext.cs
@@ -7,8 +7,8 @@ namespace Mordle.Data;
 public class ProdDbContext : IdentityDbContext<IdentityUser>
 {
     protected readonly IConfiguration _configuration;
-    public ProdDbContext(DbContextOptions<DbContext> options, IConfiguration configuration)
-        : base(options)
+    public ProdDbContext(IConfiguration configuration)
+        : base()
     {
         _configuration = configuration;
     }
@@ -19,11 +19,5 @@ public class ProdDbContext : IdentityDbContext<IdentityUser>
         // Customize the ASP.NET Identity model and override the defaults if needed.
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
-    }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-    {
-        // connect to sql server database
-        options.UseSqlServer(_configuration.GetConnectionString("WebApiDatabase"));
     }
 }
