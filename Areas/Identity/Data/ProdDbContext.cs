42c5fbe434ed8cf8ac3844d016c4916477b4fff5
Quentin Neves <neves.quentin@gmail.com>
2022-02-17 16:29:15 +0100

    Remove comments in DbContexts

--- Areas/Identity/Data/ProdDbContext.cs
+++ Areas/Identity/Data/ProdDbContext.cs
@@ -16,14 +16,10 @@ public class ProdDbContext : IdentityDbContext<IdentityUser>
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        // Customize the ASP.NET Identity model and override the defaults if needed.
-        // For example, you can rename the ASP.NET Identity table names and more.
-        // Add your customizations after calling base.OnModelCreating(builder);
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
-        // connect to sql server database
         options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
     }
 }
