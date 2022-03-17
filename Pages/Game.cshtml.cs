c13015115be562a715d72cba17abb92065fe958e
Quentin Neves <neves.quentin@gmail.com>
2022-03-17 09:07:33 +0100

    Create game page

---
+++ Pages/Game.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mordle.Pages;
+
+public class GameModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public GameModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
