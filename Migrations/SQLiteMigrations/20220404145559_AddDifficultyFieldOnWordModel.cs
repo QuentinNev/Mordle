bb026ce2cfc51557977fc942028ef1b93dd1f545
Quentin Neves <neves.quentin@gmail.com>
2022-04-04 21:56:06 +0200

    Add new entry in database with migration

---
+++ Migrations/SQLiteMigrations/20220404145559_AddDifficultyFieldOnWordModel.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mordle.Migrations.SQLiteMigrations
+{
+    public partial class AddDifficultyFieldOnWordModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "difficulty",
+                table: "Words",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Words",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "difficulty",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Words",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "difficulty",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Words",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "difficulty",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Words",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "difficulty",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Words",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "difficulty",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Words",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "difficulty",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Words",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "difficulty",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Words",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "difficulty",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Words",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "difficulty",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Words",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "difficulty",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Words",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "difficulty",
+                value: 1);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "difficulty",
+                table: "Words");
+        }
+    }
+}
--- Models/Word.cs
+++ Models/Word.cs
@@ -7,10 +7,12 @@ public class Word
     [Key]
     public int Id { get; set; }
     public string word { get; set; }
+    public int difficulty { get; set; }
 
-    public Word(int Id, string word)
+    public Word(int Id, string word, int difficulty = 1)
     {
         this.Id = Id;
         this.word = word;
+        this.difficulty = difficulty;
     }
 }
